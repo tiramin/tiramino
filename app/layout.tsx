cb2f4b36f2203997badc43dc4e787f41659bd9c1
mkk <mk1210k@gmail.com>
2024-09-25 14:43:25 +0900

    chore: Next.js 설치 및 Tailwind, Eslint, Prettier 설정 추가

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+
+import '../styles/globals.css'
+
+export const metadata: Metadata = {
+  title: `mkk's 개발 스토리`,
+  description: `프론트엔드 개발자 mkk의 기술 블로그입니다. \n 주요 기술 스택: React, Next.js, Typescript`,
+  keywords: [
+    '웹 개발자',
+    '프론트엔드 개발자',
+    'React',
+    'Next.js',
+    'Typescript',
+  ],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
